94193094aa18359db6f8457aba3b9b3cc4996adb
Rachele-Turtora <racheleturtora21@gmail.com>
2024-04-24 21:09:19 +0200

    [UPD] bonus comments and checks

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -1,26 +1,45 @@
 "use strict"
 
+// Taking user input
 const userChoice = prompt("scegli pari o dispari")
-const userNum = Number(prompt("scegli un numero da 1 a 9"))
-const computerNum = Math.floor(Math.random() * 9) + 1
+if (userChoice === "pari" || userChoice === "dispari") {
 
-console.log("Hai scelto", userChoice)
-console.log("Il tuo numero è: ", userNum)
-console.log("Il mio numero è: ", computerNum)
+    const userNum = Number(prompt("scegli un numero da 1 a 9"))
+    if ((!isNaN(userNum)) && (userNum >= 1 && userNum <= 9)) {
 
-let final
+        // Generating random number
+        const computerNum = Math.floor(Math.random() * 9) + 1
 
-if (((userNum + computerNum) % 2) === 0) {
-    final = "pari"
+        console.log("Hai scelto", userChoice)
+        console.log("Il tuo numero è: ", userNum)
+        console.log("Il mio numero è: ", computerNum)
+
+        // Calculating the sum
+        const sum = userNum + computerNum
+
+        let final
+
+        // Defining if the sum is even or odd
+        if (((sum) % 2) === 0) {
+            final = "pari"
+        } else {
+            final = "dispari"
+        }
+
+        console.log(`La somma è ${sum}, un numero ${final}`)
+
+        // Definig who the winner is
+        if (final === userChoice) {
+            console.log("Bravo, hai vinto!")
+        } else {
+            console.log("Peccato, hai perso!")
+        }
+
+    } else {
+        console.log("Scegli un numero da 1 a 9")
+    }
+
 } else {
-    final = "dispari"
-}
-
-console.log("La somma è", final)
-
-if (final === userChoice) {
-    console.log("Bravo, hai vinto!")
-} else {
-    console.log("Ho vinto io")
+    console.log("Scrivi 'pari' o 'dispari'")
 }
 
